d24cb7228bb7de8303766c4df115139703975524
Made Artha <58123759+bcmediac15@users.noreply.github.com>
2020-06-11 20:14:30 +0800

    Update konfigurasi.php
    Menambhakan catatan Pada bagian user name dan password mysql

--- konfigurasi.php
+++ konfigurasi.php
@@ -5,6 +5,7 @@ date_default_timezone_set("Asia/Makassar");
 //definisi koneksi ke database
 define("DBHOST","localhost");
 define("DBNAME","db_kampus");
+//userName dan Password disesuaikan dengan setting DMBS Mysql
 define("DBUSER","stiki");
 define("DBPWD","web1");
 ?>
